40b67b6d6bf419ae5cb4e9149e33b96ab10debf0
kashafzahida <khanabdullah4289951@gmail.com>
2019-11-22 23:11:00 +0500

    added search

--- app/src/main/java/com/example/jbconsbiosystem/AttendenceDashboard.java
+++ app/src/main/java/com/example/jbconsbiosystem/AttendenceDashboard.java
@@ -1,10 +1,15 @@
 package com.example.jbconsbiosystem;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,12 +27,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class AttendenceDashboard extends AppCompatActivity {
+public class AttendenceDashboard extends AppCompatActivity  {
 
     private RecyclerView recyclerView;
     private EmployeeRecycler adaptor;
     private List<EmployeeModel> modelClassList;
-
+    private SearchView searchView;
+    // saerch pe click nai horha keyboard open ho raha
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +41,56 @@ public class AttendenceDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_attendence_dashboard);
 
 
-
+         searchView=findViewById(R.id.searchclass);
         modelClassList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_total_users);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
         recyclerLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(recyclerLayoutManager);
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        imm.showSoftInput(view, 0);
+                    }
+                }
+            }
+        });
 
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.e("onQueryTextChange", "called");
+                return false;
+            }
 
-         AddDataToRecyclerView();
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                AddDataToRecyclerView(query);
+
+                // Do your task here
+
+                return false;
+            }
+
+        });
+
+         AddDataToRecyclerView("");
     }
 
-    private void AddDataToRecyclerView() {
-        String url= Urls.attendance+"?screen=GetUsers";
+    private void AddDataToRecyclerView( String name) {
+        String url= Urls.attendance+"?screen=GetUsersSearch&name="+name;
         VolleyRequest.GetRequest(AttendenceDashboard.this, url, new VolleyPostCallBack() {
             @Override
             public void OnSuccess(JSONObject jsonObject) {
                 Log.e("yes", "OnSuccess: " );
+
                 JSONArray jsonArray=new JSONArray();
                 try {
                     jsonArray=jsonObject.getJSONArray("result");
@@ -84,5 +120,6 @@ public class AttendenceDashboard extends AppCompatActivity {
 
 
     }
+
 
 }
